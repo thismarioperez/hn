ab6c51c766a2d7c4d1ce9344ce1b6f9ae4433071
Mario Perez <mario.perez415@gmail.com>
2024-09-25 23:13:17 -0400

    Setup a 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,14 @@ import { routeTree } from "@/routeTree.gen";
 import { TanStackRouterDevtools } from "@/lib/tanstack-router/components";
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({
+    routeTree,
+    defaultNotFoundComponent: () => (
+        <>
+            <h1>404 - Not Found</h1>
+        </>
+    ),
+});
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
